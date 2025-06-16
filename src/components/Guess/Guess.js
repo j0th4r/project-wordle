dfb271df0ad1384db544f01482d2056face46780
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-06-16 23:57:36 +0800

    feat: add guess slots and implement game logic

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
+
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell'
+
+  return (
+    <span className={className}>{letter}</span>
+  )
+}
+
+function Guess({ value, answer }) {
+  const results = checkGuess(value, answer)
+
+  return (
+    <p className="guess">
+      {range(5).map((num) => (
+        <Cell 
+          key={num}
+          letter={results ? results[num].letter : undefined}
+          status={results ? results[num].status : undefined}
+        />
+      ))}
+    </p>
+
+  );
+}
+
+export default Guess;
